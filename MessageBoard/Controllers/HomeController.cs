d6bfaf87707fcc88d6f50bc51b4d0f5e36dd3cf5
Vasilut Lucian <vasilut@outlook.com>
2017-01-31 20:31:09 +0200

    Add project files.

---
+++ MessageBoard/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using MessageBoard.Models;
+using MessageBoard.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MessageBoard.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IMailService _mail;
+
+        public HomeController(IMailService mail)
+        {
+            _mail = mail;
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(ContactModel model)
+        {
+            var msg = string.Format("Comment From: {1}{0}Email:{2}{0}Website:{3}{0} Comment",
+                             Environment.NewLine,
+                             model.Name,
+                             model.Email,
+                             model.Website,
+                             model.Comment);
+
+           
+            if(_mail.SendMail("noreply@yourdomain.com",
+                        "lucian.vasilut10@gmail.com",
+                        "Web Contact", msg))
+            {
+                ViewBag.MailSent = true;
+            }
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult MyMessages()
+        {
+            return View();
+        }
+    }
+}
