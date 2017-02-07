f0cb4588e8aa03a1aa7fe67eedbf3836d8452fec
Vasilut Lucian <vasilut@outlook.com>
2017-02-07 20:00:23 +0200

    Added angular in project + controller.

---
+++ MessageBoard/js/home-index.es5.js
@@ -0,0 +1,12 @@
+﻿//home-index.js
+'use strict';
+var myApp = angular.module("appLuc", []);
+
+(function (app) {
+    "use strict";
+    app.controller("homeIndexController", function ($scope, $http) {
+        $scope.name = "Lucian";
+        alert("Hello Lucian");
+    });
+})(myApp);
+
---
+++ MessageBoard/js/second-index.js
@@ -0,0 +1,28 @@
+﻿//home-index.js
+'use strict';
+var app = angular.module("appLuc", []);
+
+(function (app) {
+    "use strict";
+    app.controller("secondIndexController", function ($scope, $http) {
+        $scope.name = "Lucian Vasilut";
+
+        $scope.data = [
+            {
+                title: "This is a message",
+                body: "I'm right about this",
+                created: "4/24/2013"
+            },
+            {
+                title: "This is a second message",
+                body: "I'm right about this second",
+                created: "4/24/2013"
+            },
+            {
+                title: "This is a third message",
+                body: "I'm right about this third",
+                created: "4/24/2013"
+            },
+        ]
+    });
+})(app);
